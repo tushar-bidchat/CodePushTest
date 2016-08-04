93683fd9088293f27060e702c76d452d7c16f3f9
Tushar Vengurlekar <tushar.vengurlekar@bidchat.com>
2016-08-04 12:12:34 +0530

    Update 222

--- www/js/index.js
+++ www/js/index.js
@@ -27,10 +27,6 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        
-        document.addEventListener("resume", function () {
-            codePush.sync();
-        });
     },
     // deviceready Event Handler
     //
